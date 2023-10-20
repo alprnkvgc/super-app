9e5afb86321b4fb8d51e0022d3b44ddfe4b40bb0
alprnkvgc <alperen.kavgaci@hotmail.com>
2023-10-20 22:44:21 +0300

    application layer added

---
+++ src/Application/Mappings/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Products.Commands;
+using Application.Features.Products.Queries.GetAll;
+using Application.Features.Products.Queries.GetById;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductsResponse, Product>().ReverseMap();
+            CreateMap<GetProductByIdResponse, Product>().ReverseMap();
+            CreateMap<CreateUpdateProductCommand, Product>().ReverseMap();
+        }
+    }
+}
