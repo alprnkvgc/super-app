a2ca1af9b17c42b9713aafb4b3aa9403cef13308
alprnkvgc <alperen.kavgaci@hotmail.com>
2023-10-20 22:43:57 +0300

    client.ınf layed added

---
+++ src/Client.Infrastructure/Managers/Product/IProductManager.cs
@@ -0,0 +1,11 @@
+﻿using Application.Features.Products.Queries.GetAll;
+using Application.Features.Products.Queries.GetById;
+
+namespace Client.Infrastructure.Managers.Product
+{
+    public interface IProductManager : IManager
+    {
+        Task<List<ProductsResponse>> GetAllAsync();
+        Task<GetProductByIdResponse> GetByIdAsync();
+    }
+}
---
+++ src/Client.Infrastructure/Routes/ProductRoutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Infrastructure.Routes
+{
+    public static class ProductRoutes
+    {
+        public static string GetAll = "api/v1/Product";
+        public static string Delete = "api/v1/products";
+        public static string Save = "api/v1/products";
+        public static string GetCount = "api/v1/products/count";
+
+        public static string GetById(int id)
+        {
+            return $"api/v1/products/{id}";
+        }
+    }
+}
